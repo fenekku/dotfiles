a73e220bba998cc1c0e889933c97e52c03747b2f
fenekku <fenekku@fenekku.com>
2015-03-26 18:03:15 -0400

    update packages, keymap and UTF-8 as default encoding

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,11 +1,13 @@
 
-set -x PATH ~/.nimble/bin $PATH
-set -x PATH ~/usr/local $PATH
-set -x PATH ~/.rbenv/bin $PATH
+set PATH ~/.nimble/bin $PATH
+set PATH ~/usr/local $PATH
+set PATH ~/.rbenv/bin $PATH
 
 set -g VIRTUALFISH_COMPAT_ALIASES
 
-#. (rbenv init -|psub)
+. (rbenv init -|psub)
+
+alias betty="~/dev/ruby-dev/betty/main.rb"
 
 . $HOME/.config/fish/functions/fish_prompt.fish
 . $HOME/.config/fish/functions/ll.fish
