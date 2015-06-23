d82a94d66b3ea3662427451d821b2653515acdd2
fenekku <fenekku@fenekku.com>
2015-06-23 14:35:58 -0400

    set env variables to be accessible outside the shell

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,12 +1,16 @@
 
-#reverse order of precedence
-set PATH ~/.nimble/bin $PATH
-#nim path
-set PATH ~/dev/nim-dev/nim/bin $PATH
-set PATH ~/usr/local $PATH
-set PATH ~/.rbenv/bin $PATH
+# Place your environment variables in ~/.profile
+# that file is read on graphical start
 
-set -g VIRTUALFISH_COMPAT_ALIASES
+set PATH ~/.rbenv/bin $PATH
+# WANT: one place to set environment variables accessible to ALL software
+# APPARENT SOLUTION: put those in ~/.profile
+# PROBLEM: this works for everything except fish itself!
+#          `if type rbenv` does not find rbenv even though it is
+#          in the PATH by ~/.profile
+# STOPGAP SOLUTION: set ~/.rbenv/bin here
+# REMAINING PROBLEM: ~/.rbenv/bin and thus the shims are not seen outside
+#                    a fish shell
 
 if type rbenv > /dev/null
     . (rbenv init -|psub)
@@ -15,5 +19,6 @@ end
 
 . $HOME/.config/fish/functions/fish_prompt.fish
 . $HOME/.config/fish/functions/ll.fish
+set -g VIRTUALFISH_COMPAT_ALIASES
 . $HOME/.config/virtualfish/virtual.fish
 . $HOME/.homesick/repos/homeshick/homeshick.fish
