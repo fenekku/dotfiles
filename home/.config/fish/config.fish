95ee7aab4fbc58ebd05a16a4bec97fe24dd50af9
fenekku <fenekku@fenekku.com>
2016-08-02 09:12:31 -0400

    Comment out rbenv+fish 2.3.0 conflict for now

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -2,7 +2,9 @@
 # Place your environment variables in ~/.profile
 # that file is read on graphical start
 
-set PATH ~/.rbenv/bin $PATH
+# fish 2.3.0 makes this BREAK
+# FOR NOW JUST COMMENT IT OUT
+# set PATH ~/.rbenv/bin $PATH
 # WANT: one place to set environment variables accessible to ALL software
 # APPARENT SOLUTION: put those in ~/.profile
 # PROBLEM: this works for everything except fish itself!
@@ -11,10 +13,9 @@ set PATH ~/.rbenv/bin $PATH
 # STOPGAP SOLUTION: set ~/.rbenv/bin here
 # REMAINING PROBLEM: ~/.rbenv/bin and thus the shims are not seen outside
 #                    a fish shell
-
-if type rbenv > /dev/null
-    . (rbenv init -|psub)
-end
+# if type rbenv > /dev/null
+#     . (rbenv init -|psub)
+# end
 
 . $HOME/.homesick/repos/homeshick/homeshick.fish
 
