8e7e4e4f172fb0ccec95e17203b3ebbc249b50d6
fenekku <fenekku@fenekku.com>
2018-06-26 10:54:12 -0500

    Add containers alias

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -11,10 +11,14 @@ set --export PATH $HOME/.pyenv/bin $PATH
 status --is-interactive; and . (pyenv init -|psub)
 status --is-interactive; and . (pyenv virtualenv-init -|psub)
 
+# Docker
+alias containers "docker ps --all --format 'table {{.ID}}\t{{.Image}}\t{{.Status}}\t{{.Command}}'"
 alias dc docker-compose
 
 # Bash on Windows does not read the .profile
-# So we need to modify PATH here if necessary
+# So we need to modify PATH here if necessary.
+# This is fine since environment variables from Bash on Windows
+# are never used for GUIs anyway.
 if not contains $HOME/bin $PATH
   set --export PATH $HOME/bin $HOME/.local/bin $HOME/dev/nim-dev/nim/bin /usr/local/heroku/bin $PATH
 end
